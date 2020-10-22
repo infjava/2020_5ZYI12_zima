8b0dc09976ba42c4c1a6dff91008e96505f98a0a
Jan Janech <jan.janech@kst.uniza.sk>
2020-10-22 11:22:19 +0200

    04a - 04a - dvanasthodinovy format pomocou dolnej hranice

--- digitalneHodiny/DigitalneHodiny.java
+++ digitalneHodiny/DigitalneHodiny.java
@@ -12,10 +12,9 @@ public class DigitalneHodiny {
      * polnoci (00:00).
      */
     public DigitalneHodiny() {
-        this.hodiny = new CiselnyDisplej(13);
-        this.minuty = new CiselnyDisplej(60);
-        this.sekundy = new CiselnyDisplej(60);
-        this.hodiny.setHodnota(12);
+        this.hodiny = new CiselnyDisplej(1, 13);
+        this.minuty = new CiselnyDisplej(0, 60);
+        this.sekundy = new CiselnyDisplej(0, 60);
     }
     
     /**
@@ -29,10 +28,6 @@ public class DigitalneHodiny {
             
             if (this.minuty.getHodnota() == 0) {
                 this.hodiny.krok();
-                
-                if (this.hodiny.getHodnota() == 0) {
-                    this.hodiny.setHodnota(1);
-                }
             }
         }
     }
@@ -44,9 +39,7 @@ public class DigitalneHodiny {
      * @param minuta Minuta na ktoru sa ma cas nastavit.
      */
     public void setCas(int hodina, int minuta, int sekunda) {
-        if (hodina != 0) {
-            this.hodiny.setHodnota(hodina);
-        }
+        this.hodiny.setHodnota(hodina);
         this.minuty.setHodnota(minuta);
         this.sekundy.setHodnota(sekunda);
     }
