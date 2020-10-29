a2cd8c15bca2d1a5ed4b01e20872bead60a644f6
Jan Janech <jan.janech@kst.uniza.sk>
2020-10-29 10:44:43 +0100

    05b - trieda pre Pod - aj lavy aj pravy

---
+++ pong/Pad.java
@@ -0,0 +1,18 @@
+public class Pad {
+    private Obdlznik grafika;
+    
+    public Pad(boolean stranaPrava) {
+        this.grafika = new Obdlznik();
+        
+        this.grafika.zmenStrany(5, 50);
+        
+        this.grafika.posunZvisle(75);
+        if (stranaPrava) {
+            this.grafika.posunVodorovne(235);
+        } else {
+            this.grafika.posunVodorovne(-60);
+        }
+        
+        this.grafika.zobraz();
+    }
+}
