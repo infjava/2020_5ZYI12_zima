601f447dd1e4193b91454888509ac0255afa8e4b
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-04 14:24:55 +0100

    06a - lopticka na nahodnej pozicii a v nahodnom smere

--- pong/Lopticka.java
+++ pong/Lopticka.java
@@ -1,20 +1,42 @@
+import java.util.Random;
+
 public class Lopticka {
     private Kruh grafika;
     private int smerX;
     private int smerY;
     
     public Lopticka() {
+        Random nahodneCisla = new Random();
+        
         this.grafika = new Kruh();
         
         this.grafika.zmenPriemer(10);
         
+        int poziciaY = nahodneCisla.nextInt(261) + 20;
+        
         this.grafika.posunVodorovne(125);
-        this.grafika.posunZvisle(85);
+        this.grafika.posunZvisle(poziciaY - 60);
         
         this.grafika.zobraz();
         
-        this.smerX = -5;
-        this.smerY = 5;
+        switch (nahodneCisla.nextInt(4)) {
+            case 0:
+                this.smerX = -5;
+                this.smerY = 5;
+                break;
+            case 1:
+                this.smerX = 5;
+                this.smerY = 5;
+                break;
+            case 2:
+                this.smerX = -5;
+                this.smerY = -5;
+                break;
+            default:
+                this.smerX = 5;
+                this.smerY = -5;
+                break;
+        }
     }
     
     public void posunSa() {
