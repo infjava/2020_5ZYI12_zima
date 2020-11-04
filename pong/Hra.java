9b3a68776c73589436534668d30d0b6d1a146f71
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-04 15:03:21 +0100

    06a - trieda Hra - kompozit pre celu hru pong

---
+++ pong/Hra.java
@@ -0,0 +1,37 @@
+public class Hra {
+    private Lopticka lopticka;
+    private Pad lavyPad;
+    private Pad pravyPad;
+    private Manazer manazer;
+    
+    public Hra() {
+        this.lopticka = new Lopticka();
+        this.lavyPad = new Pad(false);
+        this.pravyPad = new Pad(true);
+        this.manazer = new Manazer();
+    }
+    
+    public void tik() {
+        this.lopticka.posunSa();
+    }
+    
+    public void posunHore() {
+        this.lavyPad.posunHore();
+    }
+    
+    public void posunDole() {
+        this.lavyPad.posunDole();
+    }
+    
+    public void posunVlavo() {
+        this.pravyPad.posunHore();
+    }
+    
+    public void posunVpravo() {
+        this.pravyPad.posunDole();
+    }
+    
+    public void start() {
+        this.manazer.spravujObjekt(this);
+    }
+}
