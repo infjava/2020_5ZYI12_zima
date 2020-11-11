cea593e603b9966d4accf26eb4391a411940e5be
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-11 15:09:09 +0100

    07a - korektne kontrolne cislice pre IBAN

--- banka/Banka.java
+++ banka/Banka.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.math.BigInteger;
 
 public class Banka {
     private String kodBanky;
@@ -10,9 +11,12 @@ public class Banka {
     public Ucet zalozUcet(String menoVlastnika) {
         Random nahodneCisla = new Random();
         
-        int kontrolneCislice = 68;
         int bban1 = nahodneCisla.nextInt(100);
         int bban2 = nahodneCisla.nextInt(100000000);
+        
+        BigInteger ibanPreVypocet = new BigInteger(String.format("%s000000%02d%08d282000", this.kodBanky, bban1, bban2));
+        int kontrolneCislice = 98 - ibanPreVypocet.mod(new BigInteger("97")).intValue();
+        
         String iban = String.format("SK%02d%s000000%02d%08d", kontrolneCislice, this.kodBanky, bban1, bban2);
         
         return new Ucet(menoVlastnika, iban);
