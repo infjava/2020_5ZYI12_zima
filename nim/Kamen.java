bea12d65cb4440bf1ed32b0376b338724cb0c843
Jan Janech <jan.janech@kst.uniza.sk>
2020-11-26 11:44:30 +0100

    09b - posun kamena na lubovolnu poziciu

--- nim/Kamen.java
+++ nim/Kamen.java
@@ -1,11 +1,17 @@
 public class Kamen {
     private Kruh kamen;
     
+    private int poziciaStlpec;
+    private int poziciaRiadok;
+    
     public Kamen(Sachovnica sachovnica) {
         this.kamen = new Kruh();
         this.kamen.zmenPriemer(18);
         this.kamen.posunVodorovne(-19 + (sachovnica.getSirka() - 1) * 20);
         this.kamen.posunZvisle(-59);
+        
+        this.poziciaStlpec = sachovnica.getSirka();
+        this.poziciaRiadok = sachovnica.getVyska();
         
         if (sachovnica.getJeZobrazena()) {
             this.kamen.zobraz();
@@ -18,5 +24,16 @@ public class Kamen {
     
     public void skry() {
         this.kamen.skry();
+    }
+    
+    public void posunSa(int riadok, int stlpec) {
+        int posunStlpce = stlpec - this.poziciaStlpec;
+        int posunRiadky = riadok - this.poziciaRiadok;
+        
+        this.kamen.posunVodorovne(posunStlpce * 20);
+        this.kamen.posunZvisle(-posunRiadky * 20);
+        
+        this.poziciaRiadok = riadok;
+        this.poziciaStlpec = stlpec;
     }
 }
