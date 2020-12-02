37acdf25f50f815ce53ea1c6f0ec0f2a36de8819
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-02 15:16:08 +0100

    10a - meno hraca na tahu - prva moznost implementacie

--- nim/Nim.java
+++ nim/Nim.java
@@ -1,11 +1,21 @@
 public class Nim {
     private Kamen kamen;
     private Sachovnica sachovnica;
+    private String menoPrveho;
+    private String menoDruheho;
+    private String menoHracaNaTahu;
     
-    public Nim(int sirkaSachovnice, int vyskaSachovnice) {
+    public Nim(int sirkaSachovnice, int vyskaSachovnice, String menoPrveho, String menoDruheho) {
         this.sachovnica = new Sachovnica(sirkaSachovnice, vyskaSachovnice);
         this.kamen = sachovnica.polozKamen();
         this.sachovnica.zobraz();
+        this.menoPrveho = menoPrveho;
+        this.menoDruheho = menoDruheho;
+        this.menoHracaNaTahu = menoPrveho;
+    }
+    
+    public String getMenoHracaNaTahu() {
+        return this.menoHracaNaTahu;
     }
     
     public void posunDole(int pocetPolicok) {
@@ -21,6 +31,11 @@ public class Nim {
         }
         
         this.kamen.posunSa(novyRiadok, novyStlpec);
+        
+        if (this.menoHracaNaTahu.equals(this.menoPrveho))
+            this.menoHracaNaTahu = this.menoDruheho;
+        else
+            this.menoHracaNaTahu = this.menoPrveho;
     }
     
     public void posunVlavo(int pocetPolicok) {
@@ -36,5 +51,10 @@ public class Nim {
         }
 
         this.kamen.posunSa(novyRiadok, novyStlpec);
+        
+        if (this.menoHracaNaTahu.equals(this.menoPrveho))
+            this.menoHracaNaTahu = this.menoDruheho;
+        else
+            this.menoHracaNaTahu = this.menoPrveho;
     }
 }
