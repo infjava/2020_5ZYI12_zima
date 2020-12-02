18bb69351f0a4e9b60e8be3ad47d8bfefc62f7d1
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-02 14:54:46 +0100

    10a - kontrola pravidiel

--- nim/Nim.java
+++ nim/Nim.java
@@ -9,15 +9,31 @@ public class Nim {
     }
     
     public void posunDole(int pocetPolicok) {
+        if (pocetPolicok <= 0) {
+            return;
+        }
+        
         int novyRiadok = this.kamen.getRiadok() - pocetPolicok;
         int novyStlpec = this.kamen.getStlpec();
+        
+        if (novyRiadok < 1) {
+            return;
+        }
         
         this.kamen.posunSa(novyRiadok, novyStlpec);
     }
     
     public void posunVlavo(int pocetPolicok) {
+        if (pocetPolicok <= 0) {
+            return;
+        }
+        
         int novyRiadok = this.kamen.getRiadok();
         int novyStlpec = this.kamen.getStlpec() - pocetPolicok;
+        
+        if (novyStlpec < 1) {
+            return;
+        }
 
         this.kamen.posunSa(novyRiadok, novyStlpec);
     }
