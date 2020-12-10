2424fb61bcb696f782e87d2bdc88458b2306f75b
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-10 11:32:07 +0100

    11b - posun tetromina vlavo/vpravo

--- tetris/Tetromino.java
+++ tetris/Tetromino.java
@@ -11,6 +11,27 @@ public class Tetromino {
         this.poziciaY = 0;
     }
     
+    public void posunVlavo() {
+        this.zmaz();
+        this.poziciaX--;
+        this.nakresli();
+    }
+    
+    public void posunVpravo() {
+        this.zmaz();
+        this.poziciaX++;
+        this.nakresli();
+    }
+    
+    public void zmaz() {
+        for (int y = 0; y < this.pixelySvietia.length; y++) {
+            for (int x = 0; x < this.pixelySvietia[y].length; x++) {
+                Displej displej = Displej.getInstancia();
+                displej.zmenStav(x + this.poziciaX, y + this.poziciaY, false);
+            }
+        }
+    }
+    
     public void nakresli() {
         for (int y = 0; y < this.pixelySvietia.length; y++) {
             for (int x = 0; x < this.pixelySvietia[y].length; x++) {
