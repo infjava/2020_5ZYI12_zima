141f604502343e1cd228bc43d7aef91e4fc9bda8
Jan Janech <jan.janech@kst.uniza.sk>
2020-12-10 11:13:15 +0100

    11b - tetromino sa pri vytvoreni umiestni do stredu hore

--- tetris/Tetromino.java
+++ tetris/Tetromino.java
@@ -1,15 +1,21 @@
 public class Tetromino {
     private boolean[][] pixelySvietia;
     
+    private int poziciaX;
+    private int poziciaY;
+    
     public Tetromino(boolean[][] pixelySvietia) {
         this.pixelySvietia = pixelySvietia;
+        
+        this.poziciaX = (Displej.SIRKA - this.pixelySvietia[0].length) / 2;
+        this.poziciaY = 0;
     }
     
     public void nakresli() {
         for (int y = 0; y < this.pixelySvietia.length; y++) {
             for (int x = 0; x < this.pixelySvietia[y].length; x++) {
                 Displej displej = Displej.getInstancia();
-                displej.zmenStav(x, y, this.pixelySvietia[y][x]);
+                displej.zmenStav(x + this.poziciaX, y + this.poziciaY, this.pixelySvietia[y][x]);
             }
         }
     }
